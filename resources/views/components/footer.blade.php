b5b8d69ea106111ce5221587ebf13155f5d9af47
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-28 11:53:56 +0700

    feat: footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,47 @@
+  <div class="bg-gray-950 px-36 pb-8 pt-40 text-gray-200">
+    <div class="gap-30 flex">
+      <div class="flex-3 flex flex-col gap-14">
+        <p class="text-xl font-bold">Tasty Food</p>
+        <p class="text-gray-400">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consequatur nulla nihil molestiae id enim dolores
+          alias! Consequuntur, temporibus corporis! Iste perspiciatis unde id nam ex necessitatibus neque reprehenderit
+          consequuntur repellat labore asperiores inventore, ipsam beatae exercitationem, consectetur fugiat eius!
+        </p>
+        <div class="flex gap-2">
+          <img src="{{ asset('images/logo/001-facebook@2x.png') }}" alt="facebook" class="size-10">
+          <img src="{{ asset('images/logo/002-twitter@2x.png') }}" alt="twitter" class="size-10">
+        </div>
+      </div>
+
+      <div class="flex flex-1 flex-col gap-6">
+        <p class="text-lg font-semibold">Useful links</p>
+        <a href="{{ url('/blog') }}">Blog</a>
+        <a href="{{ url('/animal') }}">Animal</a>
+        <a href="{{ url('/gallery') }}">Gallery</a>
+        <a href="{{ url('/testimonial') }}">Testimonial</a>
+      </div>
+
+      <div class="flex flex-1 flex-col gap-6">
+        <p class="text-lg font-semibold">Privacy</p>
+        <a href="{{ url('/career') }}">Karir</a>
+        <a href="{{ url('/about') }}">Tentang Kami</a>
+        <a href="{{ url('/contact') }}">Contact Us</a>
+        <a href="{{ url('/service') }}">Servis</a>
+      </div>
+
+      <div class="flex-2 flex flex-col gap-6">
+        <p class="text-lg font-semibold">Contact Info</p>
+        <a href="mailto:tastyfood@gmail.com">tastyfood@gmail.com</a>
+        <a href="tel:+6281234567890">+62 812 3456 7890</a>
+        <a href="https://www.google.com/maps/search/Kota%20Bandung">Kota Bandung, Jawa barat</a>
+      </div>
+    </div>
+
+    <p class="mt-20 text-center">Copyright ©2026 All right reserved</p>
+
+  </div>
+
+
   </body>
 
   </html>
