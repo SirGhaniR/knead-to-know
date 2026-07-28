8a56d5859328ea2c2e2cfa7c6030be0bc93ff508
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-28 08:18:03 +0700

    setup: add necessary files and ui/ux images/logo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,25 @@
-<div>
-    <!-- We must ship. - Taylor Otwell -->
-</div>
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+
+  <body class="font-montserrat text-gray-900">
+    <div class="relative overflow-hidden bg-gray-200 px-24 py-20">
+      <header>
+        <nav>
+          <ul class="flex items-center gap-16 text-xl">
+            <a href="{{ url('/') }}" class="class= text-4xl font-bold"z-10"">TASTY FOOD</a>
+            <a href="{{ url('/') }}" class="z-10">HOME</a>
+            <a href="{{ url('/about') }}" class="z-10">TENTANG</a>
+            <a href="{{ url('/news') }}" class="z-10">BERITA</a>
+            <a href="{{ url('/gallery') }}" class="z-10">GALERI</a>
+            <a href="{{ url('/contact') }}" class="z-10">KONTAK</a>
+          </ul>
+        </nav>
+      </header>
+      <img src="{{ asset('images/food_png/img-4-2000x2000.png') }}" alt="bg-image"
+        class="size-250 -right-65 absolute bottom-5 z-0">
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-header></x-header>
+<x-hero--home></x-hero--home>
+<x-about--home></x-about--home>
+
+<div class="p-35 flex gap-5 bg-gray-900 bg-cover bg-center">
+  <div class="py-15 relative rounded-2xl bg-gray-50 px-10 pt-40 text-center">
+    <p class="mb-5 text-4xl font-bold">LOREM IPSUM</p>
+    <p class="text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, commodi nihil quos autem vero eaque.
+    </p>
+    <img src="{{ asset('images/food_png/img-1.png') }}" alt="food_png"
+      class="size-70 absolute -top-1/2 left-0 right-0 mx-auto">
+  </div>
+</div>
+
+<x-footer></x-footer>
