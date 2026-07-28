381d41418ec6b61db7157452a539a368e61b4b52
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-28 10:51:21 +0700

    fix: forgot to add the cta button in the gallery section

--- resources/views/components/gallery--home.blade.php
+++ resources/views/components/gallery--home.blade.php
@@ -1,9 +1,12 @@
-<div class="py-22 px-20">
-  <p class="mb-22 text-center text-xl font-bold">GALERI KAMI</p>
+<div class="py-22 flex flex-col gap-20 px-20">
+  <p class="text-center text-xl font-bold">GALERI KAMI</p>
   <div class="grid grid-cols-3 gap-2">
     <div class="overflow-hidden rounded-xl shadow-sm">
       <img src="{{ asset('images/foods/jonathan-borba-Gkc_xM3VY34-unsplash.jpg') }}" alt="food-photo"
         class="aspect-square object-cover">
     </div>
   </div>
+
+  <a href="{{ url('/gallery') }}" class="px-18 text-l mx-auto bg-gray-900 py-3 font-bold text-gray-200">LIHAT LEBIH
+    BANYAK</a>
 </div>
