a24cff00b7460b256bf1e3671e1e73852b987902
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-28 08:31:31 +0700

    feat: hero and about components for home page

---
+++ resources/views/components/about--home.blade.php
@@ -0,0 +1,11 @@
+<div class="flex items-center justify-center">
+  <div class="relative flex w-2/5 flex-col gap-10 py-20 pb-40 text-center">
+    <p class="text-4xl font-bold">TENTANG KAMI</p>
+    <p class="text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim amet at laudantium ipsa nisi veniam provident
+      dignissimos iure, molestias ipsam reprehenderit minima recusandae
+      itaque alias. Temporibus porro rem facere optio!
+    </p>
+    <div class="w-35 bottom-25 absolute left-0 right-0 mx-auto h-1 bg-gray-900"></div>
+  </div>
+</div>
---
+++ resources/views/components/hero--home.blade.php
@@ -0,0 +1,16 @@
+  <div class="my-32 flex w-1/2 flex-col gap-6">
+    <div class="relative">
+      <div class="w-30 absolute -top-10 h-1 bg-gray-900"></div>
+      <p class="leading-18 text-6xl">HEALTHY <br>
+        <span class="font-extrabold">TASTY FOOD</span>
+      </p>
+    </div>
+    <p class="text-xl">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nesciunt sed eius, quis, enim possimus explicabo,
+      sapiente animi nam saepe tenetur odio obcaecati ad harum eum asperiores ipsum quos. Cumque amet mollitia
+      voluptates?
+      Aspernatur, cumque.
+    </p>
+    <a href="{{ url('/about') }}" class="px-25 w-fit bg-gray-900 py-5 text-2xl font-bold text-gray-200">TENTANG KAMI</a>
+  </div>
+  </div>
