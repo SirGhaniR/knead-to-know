5ca2867417016b626e182e58bce087b64fd3b428
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-29 13:29:51 +0700

    feat: maps embed on contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-</div>
+<x-header></x-header>
+<x-hero--universal></x-hero--universal>
+<x-form--contact></x-form--contact>
+<x-contacts--contact></x-contacts--contact>
+<x-maps--contact></x-maps--contact>
+<x-footer></x-footer>
