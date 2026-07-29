9f27a210faf000b8f56f3f9786cc8c2cc103515c
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-07-29 10:09:05 +0700

    feat: vision and mission section on about page

---
+++ resources/views/components/vision-mission--about.blade.php
@@ -0,0 +1,39 @@
+<div class="py-30 px-20">
+  <div class="flex items-center gap-16">
+    <div class="flex flex-1 gap-3">
+      <img src="{{ asset('images/foods/brooke-lark-oaz0raysASk-unsplash.jpg') }}" alt="food-image-1"
+        class="aspect-square w-1/2 rounded-2xl object-cover">
+      <img src="{{ asset('images/foods/sebastian-coman-photography-eBmyH7oO5wY-unsplash.jpg') }}" alt="food-image-2"
+        class="aspect-square w-1/2 rounded-2xl object-cover">
+    </div>
+    <div class="flex flex-1 flex-col gap-6">
+      <p class="text-xl font-bold">VISI</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tenetur praesentium ab a magnam assumenda
+        placeat
+        eum fuga debitis, unde sint reprehenderit, est voluptates odit voluptatem nesciunt atque dolorem saepe tempora
+        nihil dignissimos. Aliquid nisi quisquam quam, nostrum velit dolores dolorem, architecto dolor pariatur
+        consequuntur voluptatibus magni nam fuga. Optio, similique? Harum officiis sed ad reprehenderit corrupti,
+        perspiciatis quas fugiat ratione quo repellat atque nostrum, distinctio repudiandae ab tempore facere.
+      </p>
+    </div>
+  </div>
+
+  <div class="mt-30 flex items-center gap-16">
+    <div class="flex flex-1 flex-col gap-6">
+      <p class="text-xl font-bold">MISI</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tenetur praesentium ab a magnam assumenda
+        placeat
+        eum fuga debitis, unde sint reprehenderit, est voluptates odit voluptatem nesciunt atque dolorem saepe tempora
+        nihil dignissimos. Aliquid nisi quisquam quam, nostrum velit dolores dolorem, architecto dolor pariatur
+        consequuntur voluptatibus magni nam fuga. Optio, similique? Harum officiis sed ad reprehenderit corrupti,
+        perspiciatis quas fugiat ratione quo repellat atque nostrum, distinctio repudiandae ab tempore facere.
+      </p>
+    </div>
+    <div class="flex flex-1 gap-3">
+      <img src="{{ asset('images/foods/sanket-shah-SVA7TyHxojY-unsplash.jpg') }}" alt="food-image-1"
+        class="aspect-2/1 rounded-2xl object-cover">
+    </div>
+  </div>
+</div>
