b22d527e07693f22938dbace8bf27411f2cb8dab
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-08-02 09:39:48 +0700

    fix: use route instead of url

--- resources/views/components/section/form-contact.blade.php
+++ resources/views/components/section/form-contact.blade.php
@@ -1,6 +1,7 @@
 <div class="flex flex-col gap-10 px-20 py-24">
   <p class="text-xl font-bold">KONTAK KAMI</p>
-  <form action="{{ url('contact.store') }}" method="post" class="flex flex-col gap-6">
+  <x-ui.error />
+  <form action="{{ route('contact.store') }}" method="post" class="flex flex-col gap-6">
     @csrf
     <div class="flex gap-2">
       <div class="flex flex-1 flex-col gap-2">
@@ -12,6 +13,7 @@
         <x-ui.textarea name="message" id="message" placeholder="Message" />
       </div>
     </div>
-    <x-ui.button :href="'#'" variant="dark" full class="p-6! rounded-lg">KIRIM</x-ui.button>
+    <button type="submit"
+      class="px-18 w-full rounded-lg bg-gray-900 py-6 text-center font-bold text-gray-200 transition-colors hover:opacity-90">KIRIM</button>
   </form>
 </div>
