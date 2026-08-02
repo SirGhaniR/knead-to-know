cd1bd31deff2a23de45146938645984ce478555f
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-08-02 09:41:47 +0700

    feat: add old value for input component

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -1,4 +1,5 @@
 @props(['type' => 'text', 'name', 'id', 'placeholder' => ''])
 
 <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}"
+  value="{{ old($name) }}"
   {{ $attributes->merge(['class' => 'w-full rounded-lg px-4 py-8 outline-1 outline-gray-800']) }}>
