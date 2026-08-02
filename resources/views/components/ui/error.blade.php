c1d4353f5bba9463bffef488e12876f68137c745
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-08-02 09:40:50 +0700

    fix: correctly add border to error/success messages

--- resources/views/components/ui/error.blade.php
+++ resources/views/components/ui/error.blade.php
@@ -1,5 +1,5 @@
 @if ($errors->any())
-  <div class="mb-6 border-red-200 bg-red-50 px-4 py-3 text-red-600">
+  <div class="mb-6 border border-red-200 bg-red-50 px-4 py-3 text-red-600">
     <ul class="list-inside list-disc text-sm">
       @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>
@@ -9,13 +9,13 @@
 @endif
 
 @if (session('success'))
-  <div class="mb-6 bg-green-200 px-4 py-3 text-green-600">
+  <div class="mb-6 border border-green-200 bg-green-50 px-4 py-3 text-green-600">
     {{ session('success') }}
   </div>
 @endif
 
 @if (session('error'))
-  <div class="mb-6 border-red-200 bg-red-50 px-4 py-3 text-red-600">
+  <div class="mb-6 border border-red-200 bg-red-50 px-4 py-3 text-red-600">
     {{ session('error') }}
   </div>
 @endif
