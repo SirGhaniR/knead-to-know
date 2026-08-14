3a5764216ebdd1672e335fe5d1ff8d15b7bbc533
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-08-14 08:48:42 +0700

    refactor: right removed it from footer too

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -3,8 +3,8 @@
   <div class="lg:gap-30 flex flex-col gap-10 md:gap-12 lg:flex-row">
     <div class="md:flex-2 lg:flex-3 flex flex-1 flex-col gap-6 lg:gap-14" data-aos="fade-right" data-aos-delay="100"
       data-aos-duration="400">
-      <p class="text-xl font-bold transition duration-150 hover:scale-105" data-aos="fade-down" data-aos-delay="80"
-        data-aos-duration="300">Knead To Know</p>
+      <p class="text-xl font-bold transition" data-aos="fade-down" data-aos-delay="80" data-aos-duration="300">Knead To
+        Know</p>
       <p class="text-sm text-gray-400" data-aos="fade-up" data-aos-delay="120" data-aos-duration="400">
         Thank you for looking through our website! We hope you had a great experience here. If you encounter ANY problem
         during your visit, feel free to hit me up on the contact page or just email me with the provided mail address on
@@ -23,59 +23,54 @@
     </div>
 
     <div class="flex flex-1 flex-col gap-4 sm:gap-6" data-aos="fade-up" data-aos-delay="150" data-aos-duration="400">
-      <p class="text-base font-semibold transition duration-150 hover:scale-105 sm:text-lg" data-aos="fade-down"
-        data-aos-delay="80" data-aos-duration="300">Useful links</p>
-      <a href="#" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
+      <p class="text-base font-semibold transition sm:text-lg" data-aos="fade-down" data-aos-delay="80"
+        data-aos-duration="300">Useful links</p>
+      <a href="#" target="_blank" class="text-sm text-gray-400 transition duration-150 hover:text-gray-200"
         data-aos="fade-left" data-aos-delay="100" data-aos-duration="300">Blog</a>
-      <a href="#" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
+      <a href="#" target="_blank" class="text-sm text-gray-400 transition duration-150 hover:text-gray-200"
         data-aos="fade-left" data-aos-delay="130" data-aos-duration="300">Hewan</a>
       <a href="{{ route('gallery.index') }}" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
-        data-aos="fade-left" data-aos-delay="160" data-aos-duration="300">Galeri</a>
-      <a href="#" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
+        class="text-sm text-gray-400 transition duration-150 hover:text-gray-200" data-aos="fade-left"
+        data-aos-delay="160" data-aos-duration="300">Galeri</a>
+      <a href="#" target="_blank" class="text-sm text-gray-400 transition duration-150 hover:text-gray-200"
         data-aos="fade-left" data-aos-delay="190" data-aos-duration="300">Testimonial</a>
     </div>
 
     <div class="flex flex-1 flex-col gap-4 sm:gap-6" data-aos="fade-up" data-aos-delay="200" data-aos-duration="400">
-      <p class="text-base font-semibold transition duration-150 hover:scale-105 sm:text-lg" data-aos="fade-down"
-        data-aos-delay="80" data-aos-duration="300">Privacy</p>
-      <a href="#" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
+      <p class="text-base font-semibold transition sm:text-lg" data-aos="fade-down" data-aos-delay="80"
+        data-aos-duration="300">Privacy</p>
+      <a href="#" target="_blank" class="text-sm text-gray-400 transition duration-150 hover:text-gray-200"
         data-aos="fade-left" data-aos-delay="100" data-aos-duration="300">Karir</a>
       <a href="{{ route('about') }}" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
-        data-aos="fade-left" data-aos-delay="130" data-aos-duration="300">Tentang Kami</a>
+        class="text-sm text-gray-400 transition duration-150 hover:text-gray-200" data-aos="fade-left"
+        data-aos-delay="130" data-aos-duration="300">Tentang Kami</a>
       <a href="{{ route('contact.index') }}" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
-        data-aos="fade-left" data-aos-delay="160" data-aos-duration="300">Kontak Kami</a>
-      <a href="#" target="_blank"
-        class="text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200"
+        class="text-sm text-gray-400 transition duration-150 hover:text-gray-200" data-aos="fade-left"
+        data-aos-delay="160" data-aos-duration="300">Kontak Kami</a>
+      <a href="#" target="_blank" class="text-sm text-gray-400 transition duration-150 hover:text-gray-200"
         data-aos="fade-left" data-aos-delay="190" data-aos-duration="300">Servis</a>
     </div>
 
     <div class="lg:flex-2 flex flex-1 flex-col gap-4 sm:gap-6" data-aos="fade-left" data-aos-delay="150"
       data-aos-duration="400">
-      <p class="text-base font-semibold transition duration-150 hover:scale-105 sm:text-lg" data-aos="fade-down"
-        data-aos-delay="80" data-aos-duration="300">
+      <p class="text-base font-semibold transition sm:text-lg" data-aos="fade-down" data-aos-delay="80"
+        data-aos-duration="300">
         Contact Info
       </p>
       <a href="mailto:{{ Str::remove(' ', $contactInfo->email) }}"
-        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200 sm:gap-4"
+        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:text-gray-200 sm:gap-4"
         target="_blank" data-aos="fade-right" data-aos-delay="100" data-aos-duration="300">
         <img src="{{ asset('images/icons/mail.png') }}" alt="mail-icon" class="h-3 w-3 sm:h-4 sm:w-4">
         {{ $contactInfo->email }}
       </a>
       <a href="tel:{{ Str::remove(' ', $contactInfo->phone) }}"
-        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200 sm:gap-4"
+        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:text-gray-200 sm:gap-4"
         target="_blank" data-aos="fade-right" data-aos-delay="130" data-aos-duration="300">
         <img src="{{ asset('images/icons/phone.png') }}" alt="phone-icon" class="h-3 w-3 sm:h-4 sm:w-4">
         {{ $contactInfo->phone }}
       </a>
       <a href="{{ 'https://www.google.com/maps/search/' . $contactInfo->address }}"
-        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:translate-x-1 hover:text-gray-200 sm:gap-4"
+        class="align-center flex items-center gap-3 text-sm text-gray-400 transition duration-150 hover:text-gray-200 sm:gap-4"
         target="_blank" data-aos="fade-right" data-aos-delay="160" data-aos-duration="300">
         <img src="{{ asset('images/icons/location.png') }}" alt="location-icon"
           class="aspect-square h-3 w-3 object-contain sm:h-4 sm:w-4">
